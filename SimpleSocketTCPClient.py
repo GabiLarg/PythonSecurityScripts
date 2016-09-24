d42c70094421f4a1426a78902ebed80ed1561ec0
GabiLarg <gabygufs@gmail.com>
2016-09-24 16:04:18 -0300

    Simple socket Cliente TCP

---
+++ SimpleSocketTCPClient.py
@@ -0,0 +1,22 @@
+import socket
+
+target_host = "www.google.com"
+target_port = 80
+
+#criando um objeto socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#conectando cliente ao servidor
+
+client.connect((target_host, target_port))
+
+#enviando dados
+
+client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+#recebendo dados
+
+response = client.recv(4096)
+
+print response
